cdae409d1a63b713190e07bece2648a41bb0b320
VasylenkoKateruna <150960276+VasylenkoKateruna@users.noreply.github.com>
2023-12-25 22:34:37 +0200

    Update mindar-image.prod.js

--- weekday/mindar-image.prod.js
+++ weekday/mindar-image.prod.js
@@ -1,5 +1,5 @@
-
-import { U as i } from "./ui-85e81035.js";
+import { C as o, a as r } from "controller-495b585f.js";
+import { U as i } from "ui-85e81035.js";
 window.MINDAR || (window.MINDAR = {});
 window.MINDAR.IMAGE = {
   Controller: o,
